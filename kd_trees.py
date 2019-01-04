0cf0bde4aadb281ef4c61e5af825c8983fd4b3a1
stanleyjacob <stanleyjacob@users.noreply.github.com>
2019-01-03 23:58:01 -0800

    Update kd_trees.py

--- kd_trees.py
+++ kd_trees.py
@@ -1,7 +1,9 @@
 import numpy as np
+import sklearn
+
 np.random.seed(0)
 X = np.random.random((10, 3))
-tree = KDTree(X, leaf_size=2)
+tree = sklearn.neighbors.KDTree(X, leaf_size=2)
 dist, ind = tree.query(X[:1], k=3)
 print(ind)
 print(dist)
